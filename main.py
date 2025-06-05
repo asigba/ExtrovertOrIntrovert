020627b9d78684a5d1a298e2181bc83824d73e40
asigba <alexsigba@gmail.com>
2025-06-04 20:54:50 -0400

    manipulating dataset

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import matplotlib.pyplot as plt
 
 dataset = pd.read_csv("csv/personality_dataset.csv")
 
-counts = dataset["Personality"].value_counts()
+# Extroverts = 1, Introvert = 0
+dataset["Personality"] = (dataset["Personality"] == "Extrovert").astype(int)
+
+
+# Yes = 1, No = 0
+dataset["Drained_after_socializing"] = (dataset["Drained_after_socializing"] == "Yes").astype(int)
 
 
 
